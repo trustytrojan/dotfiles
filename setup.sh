4e0a9d5240795fcdfcd5807536b7ca9b38a60f43
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2023-09-13 00:37:17 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# Install yay if needed
+if ! type yay &> /dev/null; then
+	sudo pacman -S --needed git base-devel
+	git clone https://aur.archlinux.org/yay-bin.git
+	cd yay-bin
+	makepkg -si
+	cd ..
+	rm -rf yay-bin
+fi
+
+# Install dependencies
+yay -S --needed sway swayidle swaybg swaylock-effects waybar wofi pulseaudio pavucontrol foot grim slurp wl-clipboard dbus
+
+# Replace .config
+rm -rf ~/.config
+mv .config ~
+
+# Move wallpaper
+mv .wallpaper.jpg ~
