c8f0c8817d66290ea8fee3652edb02dcc4cbcb53
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2023-09-20 12:20:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,3 +29,6 @@ cp .wallpaper.jpg ~
 
 # Set GTK theme to Adwaita-dark
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
