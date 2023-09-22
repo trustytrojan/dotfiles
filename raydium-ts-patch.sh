38381e2c3749c37ba7bfa4af23fd90edab931018
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2023-09-22 12:03:09 -0400

    Update raydium-ts-patch.sh

--- raydium-ts-patch.sh
+++ raydium-ts-patch.sh
@@ -1,2 +1,2 @@
-# This makes the "Raydium Touchscreen" work under Arch.
+# This makes the "Raydium Touchscreen" work under Arch. Requires a restart.
 echo "blacklist raydium_i2c_ts" | sudo tee /etc/modprobe.d/unneeded-modules.conf
