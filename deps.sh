70efe1a7e405de8e0955570d38cef5917fc1232b
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-09-27 00:03:18 -0400

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -2,7 +2,7 @@
 source yay.sh
 
 # Install dependencies
-yay -Sy --needed swayfx swayidle swaybg swaylock-effects wofi wl-clipboard foot waybar otf-font-awesome grim slurp gnome-themes-extra ttc-iosevka pulseaudio pavucontrol dbus polkit-gnome gammastep libnotify qt5-wayland wireplumber
+yay -Sy --needed swayfx swayidle swaybg swaylock-effects wofi wl-clipboard foot waybar otf-font-awesome grim slurp gnome-themes-extra ttc-iosevka dbus polkit-gnome gammastep libnotify qt5-wayland wireplumber
 
 # If this PC is a laptop, install brightnessctl
 if [ -d /proc/acpi/button/lid ]; then
