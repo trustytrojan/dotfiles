cc15ec1fdf9a7f9a8b454d6068db376e6afb66a6
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-09-26 23:38:50 -0400

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 # Install yay if needed
-source install-yay.sh
+source yay.sh
 
 # Install dependencies
 yay -Sy --needed swayfx swayidle swaybg swaylock-effects wofi wl-clipboard foot waybar otf-font-awesome grim slurp gnome-themes-extra ttc-iosevka pulseaudio pavucontrol dbus polkit-gnome gammastep libnotify qt5-wayland wireplumber
