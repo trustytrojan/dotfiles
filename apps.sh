676f941ab4d1872cb54fd147e00f6eb5304e8c1e
Tarun R <87675609+trustytrojan@users.noreply.github.com>
2023-10-14 23:27:23 -0400

    Delete apps.sh

--- apps.sh
+++
@@ -1,8 +0,0 @@
-# Install yay if needed
-source yay.sh
-
-# Install apps that complete the desktop experience.
-yay -Sy --needed google-chrome htop nvtop
-
-# Chrome commandline flags to force usage of Wayland and GNOME Keyring
-cp .config/chrome-flags.conf ~/.config
