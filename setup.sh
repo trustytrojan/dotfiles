99e64ca10ac72b6842c10fa661c6d6e3984648ea
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2024-05-28 19:47:19 -0400

    Update setup.sh
    fix bad scripting practices
    remove unnecessary `read` call

--- setup.sh
+++ setup.sh
@@ -13,25 +13,24 @@ sudo cp .bashrc /etc/bash.bashrc
 copy_config nvim
 
 read -p "Install pipewire and wireplumber? [Y/n] " pw
-[ $pw = n ] || {
+[[ $pw == "n" ]] || {
 	echo "Installing pipewire and wireplumber"
 	sudo pacman $PACMAN_OPTS pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber
 }
 
 read -p "Sway, i3, or no wm? " wm
-[ $wm = sway || $wm = i3 ] || exit
+[[ $wm == "sway" || $wm == "i3" ]] || exit
 
-# Sway/i3 Shared dependencies
+# Install shared dependencies
 sudo pacman $PACMAN_OPTS gammastep gnome-themes-extra ttc-iosevka dbus polkit-gnome libnotify qpwgraph pavucontrol
 
 # Sway/i3 shared configs
 copy_config gammastep gtk-3.0
 echo "export GTK_THEME=Adwaita-dark" >> ~/.bashrc
 
-# Determine window manager
-read -p "Sway or i3? " wm
+# Install window manager + dependencies
 case $wm in
-	"sway")
+	sway)
 		# Require yay installation
 		if ! type yay >/dev/null; then
 			echo "yay is not installed. Aborting."
@@ -47,7 +46,7 @@ case $wm in
 		# Set dark theme
 		gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 
-	"i3")
+	i3)
 		# Install i3 dependencies
 		sudo pacman $PACMAN_OPTS i3 xorg-server xorg-xinit feh xterm rofi
 	
