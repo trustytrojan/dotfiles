344a6e2acd5fd727fe21c71bfc8dfe1c3ca89c25
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2024-07-01 12:03:48 -0400

    Update .screen-record.sh

--- .screen-record.sh
+++ .screen-record.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 
+NOTIF_TITLE='Screen recorder'
+alias notify-send="notify-send '$NOTIF_TITLE' $@"
+
 if pidof wf-recorder; then
-	notify-send 'Screen recorder' 'There is already an instance of wf-recorder running!'
+	notify-send 'There is already an instance of wf-recorder running!'
 	exit 1
 fi
 
-FILE="$HOME/recording_$(date +'%Y-%m-%d_%H:%M:%S.mp4')"
+# Get currently focused output
 OUTPUT="$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')"
-notify-send 'Screen recorder' "Starting! Recording output $OUTPUT"
+
+# Get PulseAudio default sink monitor
+AUDIO_DEVICE="$(pactl info | awk '/Default Sink:/ {print $3}').monitor"
+
+# Output video file
+FILE="$HOME/recording_$(date +'%Y-%m-%d_%H:%M:%S.mp4')"
+
+# wf-recorder logfile
+LOGFILE='/tmp/wf-recorder.log'
+
+notify-send "Starting! Recording output $OUTPUT"
 
 wf-recorder \
 	--codec h264_vaapi \
 	--framerate 60 \
 	--output "$OUTPUT" \
-	--audio="$(pactl info | awk '/Default Sink:/ {print $3}').monitor" \
-	--file "$FILE"
+	--audio="$AUDIO_DEVICE" \
+	--file "$FILE" \
+ 	&>$LOGFILE
 
 if [ $? -ne 0 ]; then
-	notify-send 'Screen recorder' 'wf-recorder failed to start! See /tmp/wf-recorder.log'
+	notify-send "Failed to start! See $LOGFILE"
 	exit 1
 fi
 
-notify-send 'Screen recorder' "Finished! Video saved at $FILE"
-
+notify-send "Finished! Video saved at $FILE"
