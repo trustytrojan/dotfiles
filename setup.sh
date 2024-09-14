84b372b7869775e737242df6fbf21d09fef135df
trustytrojan <t@trustytrojan.dev>
2024-09-14 11:41:02 -0400

    improve setup script

--- setup.sh
+++ setup.sh
@@ -34,10 +34,15 @@ sudo pacman $PACMAN_OPTS neovim htop
 case $(read -rp 'Sway or i3? '; echo $REPLY) in
 	sway)
 		# Require yay installation
-		if ! type yay >/dev/null; then
-			echo 'error: yay is not installed. Exiting.'
-			exit 1
-		fi
+		while ! type yay >/dev/null; do
+			read -rp 'yay is not installed, would you like to install it now? [y/n] '
+			if [ $REPLY == y ]; then
+				sh install-yay.sh
+			else
+				echo 'yay is required to install swayfx and swaylock-effects, exiting'
+				return 1; exit 1
+			fi
+		done
 
 		# Install sway packages
 		yay $PACMAN_OPTS swayfx swayidle swaybg swaylock-effects wofi wl-clipboard foot waybar otf-font-awesome grim slurp xdg-desktop-portal-wlr wf-recorder
@@ -50,7 +55,7 @@ case $(read -rp 'Sway or i3? '; echo $REPLY) in
 
 	i3)
 		# Install i3 packages
-		sudo pacman $PACMAN_OPTS i3 xorg-server xorg-drivers xorg-xinit feh xterm rofi ffmpeg
+		sudo pacman $PACMAN_OPTS i3 xorg-server xorg-drivers xorg-xinit feh xterm rofi
 
 		# Copy configs and X files
 		install_config i3
@@ -63,7 +68,7 @@ case $(read -rp 'Sway or i3? '; echo $REPLY) in
 		exit
 esac
 
-# Install shared packages
+# Install shared packages (audio server, media player, hwaccel drivers, polkit frontend)
 sudo pacman $PACMAN_OPTS gammastep gnome-themes-extra ttc-iosevka dbus polkit-gnome libnotify qpwgraph pavucontrol \
 	libva-mesa-driver mesa-vdpau pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber mpv \
  	$(lscpu | grep Intel >/dev/null && echo 'intel-media-driver libva-intel-driver')
@@ -76,3 +81,5 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
 # Copy wallpaper
 install_home .wallpaper.jpg
+
+echo 'Setup complete! Only packages essential to your chosen desktop experience were installed. Install other things (browser, games, applications) yourself using yay.'
