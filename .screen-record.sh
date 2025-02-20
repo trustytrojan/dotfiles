f7223912b6c3cb852522c1cff0a7f8aed5887471
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2025-02-20 05:04:27 +0000

    Update .screen-record.sh

--- .screen-record.sh
+++ .screen-record.sh
@@ -2,10 +2,15 @@
 # Script to make usage of wf-recorder more enjoyable using notify-send.
 
 NOTIF_TITLE='Screen recorder'
-alias notify-send="notify-send '$NOTIF_TITLE' $@"
+LOGFILE='/tmp/screen-recorder.log'
+
+output() {
+	notify-send -t 3000 "Screen recorder" "$@"
+	echo "$@" >>$LOGFILE
+}
 
 if pidof wf-recorder; then
-	notify-send 'There is already an instance of wf-recorder running!'
+	output 'There is already an instance of wf-recorder running!'
 	exit 1
 fi
 
@@ -18,22 +23,22 @@ AUDIO_DEVICE="$(pactl info | awk '/Default Sink:/ {print $3}').monitor"
 # Output video file
 FILE="$HOME/recording_$(date +'%Y-%m-%d_%H:%M:%S.mp4')"
 
-# wf-recorder logfile
-LOGFILE='/tmp/wf-recorder.log'
+output "Starting! Recording output $OUTPUT"
 
-notify-send "Starting! Recording output $OUTPUT"
-
+# Record at 720p60 using VA-API for scaling and encoding
+# Change this if your GPU does not support VA-API
 wf-recorder \
 	--codec h264_vaapi \
 	--framerate 60 \
+	--filter scale_vaapi=format=nv12,scale_vaapi=1280:720 \
 	--output "$OUTPUT" \
 	--audio="$AUDIO_DEVICE" \
 	--file "$FILE" \
- 	&>$LOGFILE
+ 	&>>"$LOGFILE"
 
 if [ $? -ne 0 ]; then
-	notify-send "Failed to start! See $LOGFILE"
+	output "Failed to start wf-recorder! See $LOGFILE"
 	exit 1
 fi
 
-notify-send "Finished! Video saved at $FILE"
+output "Finished! Video saved at $FILE"
