e965bacab4cc1b78fc945327324d43b1facea9a8
trustytrojan <t@trustytrojan.dev>
2026-03-07 11:41:10 -0500

    stop doing the "bring themes into one branch" thing

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,31 +4,39 @@ set -e
 install_config() {
 	for dir in $@; do
 		echo "Installing ~/.config/$dir"
-		cp -r .config/$dir ~/.config
+		cp -r ".config/$dir" ~/.config
 	done
 }
 
 install_etc() {
 	for file in $@; do
 		echo "Installing /etc/$file"
-		sudo cp $file /etc
+		sudo cp "$file" /etc
 	done
 }
 
 install_home() {
 	for file in $@; do
 		echo "Installing ~/$file"
-		cp $file ~
+		cp "$file" ~
 	done
 }
 
 PACMAN_OPTS='-Sy --needed'
 
 # Install non-WM-related stuff
-install_etc bash.bashrc pacman.conf
+if [[ "$(uname -m)" == "x86_64" ]]; then
+	# only do this if we are on normal arch.
+	# other architectures will not have the same package repos
+	install_etc pacman.conf
+else
+	echo "Installation of pacman.conf skipped since this is not normal Arch Linux."
+fi
+
+install_etc bash.bashrc
 install_home .bashrc
-install_config nvim htop
-sudo pacman $PACMAN_OPTS neovim htop
+install_config nvim htop nvtop
+sudo pacman $PACMAN_OPTS neovim htop nvtop
 
 # Install window manager + dependencies
 case $(read -rp 'Sway or i3? '; echo $REPLY) in
@@ -45,13 +53,36 @@ case $(read -rp 'Sway or i3? '; echo $REPLY) in
 		done
 
 		# Install sway packages
-		yay $PACMAN_OPTS swayfx swayidle swaybg swaylock-effects wofi wl-clipboard foot waybar otf-font-awesome grim slurp xdg-desktop-portal-wlr wf-recorder \
-  			qt5-wayland qt6-wayland xdg-desktop-portal-gtk
+		SWAY_PACKAGES=(
+			swayfx
+			swayidle
+			swaybg
+			swaylock-effects
+			wofi
+			wl-clipboard
+			foot
+			waybar
+			otf-font-awesome
+			grim
+			slurp
+			xdg-desktop-portal-wlr
+			xdg-desktop-portal-gtk
+			wf-recorder
+			qt5-wayland
+			qt6-wayland
+			idlehack-git
+		)
+
+		yay $PACMAN_OPTS "${SWAY_PACKAGES[@]}"
 
 		# Copy sway configs
 		install_config foot sway swayidle swaylock waybar mako
 		install_home .screen-record.sh
-		chmod u+x ~/.screen-record.sh
+
+		# Setup services
+		sudo sed -i '/^method=/s/.*/method=reallyfreegeoip/' /etc/geoclue/geoclue.conf
+		sudo systemctl enable --now geoclue
+		systemctl --user enable --now gammastep mako idlehack
 		;;
 
 	i3)
@@ -69,12 +100,36 @@ case $(read -rp 'Sway or i3? '; echo $REPLY) in
 		exit
 esac
 
-# Install shared packages: audio server, media player, hwaccel drivers, polkit frontend, file manager (nemo),
-# xorg packages for legacy software, MTP support in nemo
-sudo pacman $PACMAN_OPTS gammastep gnome-themes-extra ttc-iosevka dbus polkit-gnome libnotify qpwgraph pavucontrol \
-	libva-mesa-driver mesa-vdpau pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber mpv \
- 	$(lscpu | grep Intel >/dev/null && echo 'intel-media-driver libva-intel-driver') xorg-xhost xorg-xrandr \
-  	nemo gvfs-mtp
+# Packages needed regardless of window manager
+SHARED_PACKAGES=(
+	gammastep
+	geoclue
+	gnome-themes-extra
+	ttc-iosevka
+	dbus
+	polkit-gnome
+	libnotify
+	qpwgraph
+	pavucontrol
+	pipewire
+	pipewire-alsa
+	pipewire-pulse
+	pipewire-jack
+	wireplumber
+	mpv
+	xorg-xhost # for gparted
+	xorg-xrandr
+  	nemo
+	gvfs-mtp
+)
+
+# Add Intel VA drivers if CPU is Intel
+if lscpu | grep -q Intel; then
+	SHARED_PACKAGES+=(intel-media-driver libva-intel-driver)
+fi
+
+# Install shared packages
+sudo pacman $PACMAN_OPTS "${SHARED_PACKAGES[@]}"
 
 # Install shared configs
 install_config gammastep gtk-3.0 mpv vesktop
@@ -82,11 +137,7 @@ install_config gammastep gtk-3.0 mpv vesktop
 # Set dark theme
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
-# Setup user services
-sudo sed -i '/^method=/s/.*/method=reallyfreegeoip/' /etc/geoclue/geoclue.conf
-systemctl --user enable gammastep mako
-
 # Copy wallpaper
 install_home .wallpaper.jpg
 
-echo 'Setup complete! Only packages essential to your chosen desktop experience were installed. Install other things (browser, games, applications) yourself using yay.'
+echo 'Setup complete! Only packages essential to your chosen window manager were installed. Install other things (browser, games, applications) yourself using yay.'
